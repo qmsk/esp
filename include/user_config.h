ece1f54960065e2aa5b441863ed22f6fdd685a9c
Tero Marttila <terom@fixme.fi>
2017-08-20 16:52:18 +0300

    fixup include/user_config.h

--- include/user_config.h
+++ include/user_config.h
@@ -1,27 +1,3 @@
-/*
- * ESPRSSIF MIT License
- *
- * Copyright (c) 2015 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
- *
- * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
- * it is free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
-
 #ifndef __USER_CONFIG_H__
 #define __USER_CONFIG_H__
 
@@ -37,7 +13,14 @@ struct user_config {
   char wifi_password[64];
 };
 
-#define USER_CONFIG_WIFI_SSID "qmsk-iot24"
-#define USER_CONFIG_WIFI_PASSWORD "..."
+#include "local/user_config.h"
+
+#define USER_CONFIG_UART_BAUD_RATE 74880
+#ifndef USER_CONFIG_WIFI_SSID
+  #error "include/local/user_config.h: #define USER_CONFIG_WIFI_SSID"
+#endif
+#ifndef USER_CONFIG_WIFI_PASSWORD
+  #error "include/local/user_config.h: #define USER_CONFIG_WIFI_PASSWORD"
+#endif
 
 #endif

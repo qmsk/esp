48c42d392bf027f247e061aa52733b501e9b6fd5
Tero Marttila <terom@fixme.fi>
2017-08-21 01:23:37 +0300

    user main: print reset exc cause

--- user/user_main.c
+++ user/user_main.c
@@ -83,9 +83,18 @@ uint32 user_rf_cal_sector_set(void)
 
 void print_system()
 {
+  struct rst_info *rst_info = system_get_rst_info();
+
   printf("!\tSDK version %s\n", system_get_sdk_version());
   printf("|\tESP8266	chip ID=0x%x\n",	system_get_chip_id());
-  printf("|\tReset cause=%d\n", system_get_rst_info()->reason);
+  printf("|\tReset reason=%d (exc cause=%u)\n", rst_info->reason, rst_info->exccause);
+  printf("|\tReset exc: cause=%u epc1=%08x epc2=%08x epc3=%08x excv=%08x depc=%08x rtn=%08x\n",
+    rst_info->exccause,
+    rst_info->epc1, rst_info->epc2, rst_info->epc3,
+    rst_info->excvaddr,
+    rst_info->depc,
+    rst_info->rtn_addr
+  );
   printf("|\tBoot version=%d mode=%d addr=%d\n", system_get_boot_version(), system_get_boot_mode(), system_get_userbin_addr());
   printf("|\tFlash id=%d size_map=%d\n", spi_flash_get_id(), system_get_flash_size_map());
   printf("|\tCPU freq=%d\n", system_get_cpu_freq());

dfefe6c2ff9f0c39eec0ed695d32640e0f853da8
Tero Marttila <terom@fixme.fi>
2017-08-21 00:20:13 +0300

    logging: DEBUG is optional

--- user/include/logging.h
+++ user/include/logging.h
@@ -5,7 +5,12 @@
 
 void logging_printf(const char *prefix, const char *func, const char *fmt, ...);
 
-#define LOG_DEBUG(...)  do { logging_printf("DEBUG ", __func__, __VA_ARGS__); } while(0)
+#ifdef DEBUG
+  #define LOG_DEBUG(...)  do { logging_printf("DEBUG ", __func__, __VA_ARGS__); } while(0)
+#else
+  #define LOG_DEBUG(...)
+#endif
+
 #define LOG_INFO(...)   do { logging_printf("INFO  ", __func__, __VA_ARGS__); } while(0)
 #define LOG_WARN(...)   do { logging_printf("WARN  ", __func__, __VA_ARGS__); } while(0)
 #define LOG_ERROR(...)  do { logging_printf("ERROR ", __func__, __VA_ARGS__); } while(0)

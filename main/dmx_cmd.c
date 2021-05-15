5941308057353cae49d7864147909195b7572827
Tero Marttila <terom@fixme.fi>
2021-05-15 17:28:28 +0300

    dmx: count command for testing output

--- main/dmx_cmd.c
+++ main/dmx_cmd.c
@@ -62,6 +62,34 @@ error:
   return err;
 }
 
+int dmx_cmd_count(int argc, char **argv, void *ctx)
+{
+  int err;
+  int count;
+  uint8_t *data;
+
+  if ((err = cmd_arg_int(argc, argv, 1, &count)))
+    return err;
+
+  if (!(data = malloc(count))) {
+    LOG_ERROR("malloc");
+  }
+
+  for (int i = 0; i < count; i++) {
+    data[i] = (uint8_t) i;
+  }
+
+  if ((err = output_dmx(data, count))) {
+    LOG_ERROR("output_dmx");
+    goto error;
+  }
+
+error:
+  free(data);
+
+  return err;
+}
+
 int dmx_cmd_out(int argc, char **argv, void *ctx)
 {
   int err;
@@ -95,6 +123,7 @@ error:
 const struct cmd dmx_commands[] = {
   { "zero",   dmx_cmd_zero,       .usage = "COUNT",          .describe = "Output COUNT channels at zero" },
   { "all",    dmx_cmd_all,        .usage = "COUNT VALUE",    .describe = "Output COUNT channels at VALUE" },
+  { "count",  dmx_cmd_count,      .usage = "COUNT",          .describe = "Output COUNT channels with 0..COUNT as value" },
   { "out",    dmx_cmd_out,        .usage = "VALUE...",       .describe = "Output given VALUEs as channels" },
   { }
 };

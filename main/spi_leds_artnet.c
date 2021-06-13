08dba7068d55072360f69768d5d9a1e4011c111f
Tero Marttila <terom@fixme.fi>
2021-06-13 21:48:30 +0300

    main spi_leds: implement different artnet dmx modes

--- main/spi_leds_artnet.c
+++ main/spi_leds_artnet.c
@@ -25,7 +25,26 @@ struct spi_leds_artnet {
   struct artnet_dmx artnet_dmx;
 } *spi_leds_artnet;
 
-static int spi_leds_artnet_dmx(struct spi_leds *spi_leds, struct artnet_dmx *artnet_dmx)
+static void spi_leds_artnet_dmx_rgb(struct spi_leds *spi_leds, struct artnet_dmx *artnet_dmx)
+{
+  unsigned count = spi_leds_count(spi_leds);
+
+  LOG_DEBUG("len=%u", artnet_dmx->len);
+
+  for (unsigned i = 0; i < count && artnet_dmx->len >= (i + 1) * 3; i++) {
+    spi_leds_set(spi_leds, i, (struct spi_led_color) {
+      .r = artnet_dmx->data[i * 3 + 0],
+      .g = artnet_dmx->data[i * 3 + 1],
+      .b = artnet_dmx->data[i * 3 + 2],
+
+      .parameters = {
+        .brightness = 255,
+      }
+    });
+  }
+}
+
+static void spi_leds_artnet_dmx_bgr(struct spi_leds *spi_leds, struct artnet_dmx *artnet_dmx)
 {
   unsigned count = spi_leds_count(spi_leds);
 
@@ -42,8 +61,25 @@ static int spi_leds_artnet_dmx(struct spi_leds *spi_leds, struct artnet_dmx *art
       }
     });
   }
+}
 
-  return update_spi_leds();
+static void spi_leds_artnet_dmx_grb(struct spi_leds *spi_leds, struct artnet_dmx *artnet_dmx)
+{
+  unsigned count = spi_leds_count(spi_leds);
+
+  LOG_DEBUG("len=%u", artnet_dmx->len);
+
+  for (unsigned i = 0; i < count && artnet_dmx->len >= (i + 1) * 3; i++) {
+    spi_leds_set(spi_leds, i, (struct spi_led_color) {
+      .g = artnet_dmx->data[i * 3 + 0],
+      .r = artnet_dmx->data[i * 3 + 1],
+      .b = artnet_dmx->data[i * 3 + 2],
+
+      .parameters = {
+        .brightness = 255,
+      }
+    });
+  }
 }
 
 static void spi_leds_artnet_task(void *ctx)
@@ -53,8 +89,26 @@ static void spi_leds_artnet_task(void *ctx)
   for (;;) {
     if (!xQueueReceive(spi_leds_artnet->queue, &spi_leds_artnet->artnet_dmx, portMAX_DELAY)) {
       LOG_WARN("xQueueReceive");
-    } else if (spi_leds_artnet_dmx(spi_leds_artnet->spi_leds, &spi_leds_artnet->artnet_dmx)) {
-      LOG_WARN("spi_leds_artnet_dmx");
+      continue;
+    }
+
+    switch(spi_leds_artnet->mode) {
+      case SPI_LEDS_RGB:
+        spi_leds_artnet_dmx_rgb(spi_leds_artnet->spi_leds, &spi_leds_artnet->artnet_dmx);
+        break;
+
+      case SPI_LEDS_BGR:
+        spi_leds_artnet_dmx_bgr(spi_leds_artnet->spi_leds, &spi_leds_artnet->artnet_dmx);
+        break;
+
+      case SPI_LEDS_GRB:
+        spi_leds_artnet_dmx_grb(spi_leds_artnet->spi_leds, &spi_leds_artnet->artnet_dmx);
+        break;
+    }
+
+    if (update_spi_leds()) {
+      LOG_WARN("update_spi_leds");
+      continue;
     }
   }
 }

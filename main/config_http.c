b038f26c9fa86d121c2ee638ec1cb4fd7aa1a750
Tero Marttila <terom@fixme.fi>
2021-06-30 18:33:19 +0300

    main: POST /config.ini

--- main/config_http.c
+++ main/config_http.c
@@ -47,15 +47,74 @@ int config_get_handler(struct http_request *request, struct http_response *respo
 
   if ((err = config_write(&config, file))) {
     LOG_WARN("config_write");
-    return -1;
+    goto error;
   }
 
+error:
   if (fclose(file) < 0) {
     LOG_WARN("fclose: %s", strerror(errno));
     return -1;
   }
 
-  return 0;
+  return err;
+}
+
+int config_post_handler_ini(struct http_request *request, struct http_response *response, void *ctx)
+{
+  FILE *file;
+  int err;
+
+  if ((err = http_request_open(request, &file))) {
+    LOG_WARN("http_request_open");
+    return err;
+  }
+
+  LOG_INFO("config read...");
+  LOG_DEBUG("file=%p", file);
+
+  if ((err = config_read(&config, file))) {
+    LOG_WARN("config_read");
+    err = HTTP_UNPROCESSABLE_ENTITY;
+    goto error;
+  }
+
+  LOG_INFO("config save...");
+
+  if ((err = config_save(&config))) {
+    LOG_ERROR("config_save");
+    goto error;
+  }
+
+  err = HTTP_NO_CONTENT;
+
+error:
+  if (fclose(file) < 0) {
+    LOG_WARN("fclose: %s", strerror(errno));
+    return -1;
+  }
+
+  return err;
+}
+
+int config_post_handler(struct http_request *request, struct http_response *response, void *ctx)
+{
+  const struct http_request_headers *headers;
+  int err;
+
+  if ((err = http_request_headers(request, &headers))) {
+    LOG_WARN("http_request_headers");
+    return err;
+  }
+
+  switch (headers->content_type) {
+    case HTTP_CONTENT_TYPE_TEXT_PLAIN:
+      return config_post_handler_ini(request, response, ctx);
+
+    default:
+      LOG_WARN("Unkonwn Content-Type");
+
+      return HTTP_UNSUPPORTED_MEDIA_TYPE;
+  }
 }
 
 static int config_api_write_configtab_uint16_members(struct json_writer *w, const struct configtab *tab)
--- main/http_routes.c
+++ main/http_routes.c
@@ -1,9 +1,10 @@
 #include "http_routes.h"
 
 const struct http_route http_routes[] = {
-  { "GET", "",                http_dist_index_handler, NULL },
-  { "GET", "dist/",           http_dist_handler,       NULL },
-  { "GET", "config.ini",      config_get_handler,      NULL },
+  { "GET",  "",                http_dist_index_handler, NULL },
+  { "GET",  "dist/",           http_dist_handler,       NULL },
+  { "GET",  "config.ini",      config_get_handler,      NULL },
+  { "POST", "config.ini",      config_post_handler,     NULL },
 
   { "GET",  "api/config",      config_api_get,  NULL },
   { "POST", "api/config",      config_api_post, NULL },

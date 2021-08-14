b9983e04115b911927a3b7ed2e0ce9976633c751
Tero Marttila <terom@fixme.fi>
2021-08-14 16:16:35 +0300

    spi_leds: config rate

--- main/spi_leds.c
+++ main/spi_leds.c
@@ -8,13 +8,15 @@
 
 #include <logging.h>
 
-#define SPI_MODE (SPI_MODE_0)
-#define SPI_CLOCK (SPI_CLOCK_2MHZ)
+#define SPI_LEDS_MODE (SPI_MODE_0)
+#define SPI_LEDS_CLOCK (SPI_CLOCK_1MHZ)
+#define SPI_LEDS_PINS (SPI_PINS)
 
 struct spi_leds_config {
   bool enabled;
-  uint16_t count;
   int protocol;
+  int spi_clock;
+  uint16_t count;
 
   bool artnet_enabled;
   uint16_t artnet_universe;
@@ -22,6 +24,7 @@ struct spi_leds_config {
 };
 
 struct spi_leds_config spi_leds_config = {
+  .spi_clock   = SPI_LEDS_CLOCK,
   .protocol    = SPI_LEDS_PROTOCOL_APA102,
   .artnet_mode = SPI_LEDS_BGR,
 };
@@ -31,6 +34,23 @@ const struct config_enum spi_leds_protocol_enum[] = {
   { "P9813",  SPI_LEDS_PROTOCOL_P9813   },
   {}
 };
+
+const struct config_enum spi_leds_rate_enum[] = {
+  { "20M",  SPI_CLOCK_20MHZ   },
+  { "10M",  SPI_CLOCK_10MHZ   },
+  { "5M",   SPI_CLOCK_5MHZ    },
+  { "2M",   SPI_CLOCK_2MHZ    },
+  { "1M",   SPI_CLOCK_1MHZ    },
+  { "500K", SPI_CLOCK_500KHZ  },
+  { "200K", SPI_CLOCK_200KHZ  },
+  { "100K", SPI_CLOCK_100KHZ  },
+  { "50K",  SPI_CLOCK_50KHZ   },
+  { "20K",  SPI_CLOCK_20KHZ   },
+  { "10K",  SPI_CLOCK_10KHZ   },
+  { "1K",   SPI_CLOCK_1KHZ    },
+  {}
+};
+
 const struct config_enum spi_leds_artnet_mode_enum[] = {
   { "RGB", SPI_LEDS_RGB  },
   { "BGR", SPI_LEDS_BGR  },
@@ -42,12 +62,16 @@ const struct configtab spi_leds_configtab[] = {
   { CONFIG_TYPE_BOOL, "enabled",
     .value  = { .boolean = &spi_leds_config.enabled },
   },
-  { CONFIG_TYPE_UINT16, "count",
-    .value  = { .uint16 = &spi_leds_config.count },
-  },
   { CONFIG_TYPE_ENUM, "protocol",
     .enum_values = spi_leds_protocol_enum,
     .value       = { .enum_value = &spi_leds_config.protocol },
+  },
+  { CONFIG_TYPE_ENUM, "rate",
+    .enum_values = spi_leds_rate_enum,
+    .value       = { .enum_value = &spi_leds_config.spi_clock },
+  },
+  { CONFIG_TYPE_UINT16, "count",
+    .value  = { .uint16 = &spi_leds_config.count },
   },
   { CONFIG_TYPE_BOOL, "artnet_enabled",
     .value  = { .boolean = &spi_leds_config.artnet_enabled },
@@ -70,11 +94,13 @@ bool spi_leds_activated;
 int config_spi_master(const struct spi_leds_config *config)
 {
   struct spi_options options = {
-      .mode   = SPI_MODE,
-      .clock  = SPI_CLOCK,
-      .pins   = SPI_PINS,
+      .mode   = SPI_LEDS_MODE,
+      .clock  = config->spi_clock,
+      .pins   = SPI_LEDS_PINS,
   };
   int err;
+
+  LOG_INFO("mode=%02x clock=%u pins=%02x", options.mode, options.clock, options.pins);
 
   if ((err = spi_master_new(&spi_master, options))) {
     LOG_ERROR("spi_master_new");
@@ -92,12 +118,16 @@ int config_spi_leds(const struct spi_leds_config *config)
   };
   int err;
 
+  LOG_INFO("protocol=%u count=%u", options.protocol, options.count);
+
   if ((err = spi_leds_new(&spi_leds, spi_master, options))) {
     LOG_ERROR("spi_leds_new");
     return err;
   }
 
   if (spi_leds_config.artnet_enabled) {
+    LOG_INFO("artnet universe=%u mode=%u", spi_leds_config.artnet_universe, spi_leds_config.artnet_mode);
+
     if ((err = init_spi_leds_artnet(spi_leds_config.artnet_universe, spi_leds_config.artnet_mode))) {
       LOG_ERROR("init_spi_leds_artnet");
       return err;

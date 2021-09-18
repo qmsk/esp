597f1903ea3a33b0bb50e0ef3129154d1f70032a
Tero Marttila <terom@fixme.fi>
2021-09-19 00:39:47 +0300

    main: doc http routes implementation files

--- main/http_routes.h
+++ main/http_routes.h
@@ -5,14 +5,18 @@
 
 extern const struct http_route http_routes[];
 
+/* http_dist.c */
 int http_dist_handler(struct http_request *request, struct http_response *response, void *ctx);
 int http_dist_index_handler(struct http_request *request, struct http_response *response, void *ctx);
+
+/* config_http.c */
 int config_get_handler(struct http_request *request, struct http_response *response, void *ctx);
 int config_post_handler(struct http_request *request, struct http_response *response, void *ctx);
 
+int config_api_get(struct http_request *request, struct http_response *response, void *ctx);
+int config_api_post(struct http_request *request, struct http_response *response, void *ctx);
+
+/* system_http.c */
 int system_api_handler(struct http_request *request, struct http_response *response, void *ctx);
 int system_api_tasks_handler(struct http_request *request, struct http_response *response, void *ctx);
 int system_api_restart_handler(struct http_request *request, struct http_response *response, void *ctx);
-
-int config_api_get(struct http_request *request, struct http_response *response, void *ctx);
-int config_api_post(struct http_request *request, struct http_response *response, void *ctx);

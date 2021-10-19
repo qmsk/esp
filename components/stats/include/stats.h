a2368c04a9abb46c6db2fd318bcd2b4e65712f1c
Tero Marttila <terom@fixme.fi>
2021-10-19 23:00:45 +0300

    fixup stats get const

--- components/stats/include/stats.h
+++ components/stats/include/stats.h
@@ -26,17 +26,17 @@ static inline void stats_counter_increment(struct stats_counter *counter)
   counter->count++;
 }
 
-static inline struct stats_counter stats_counter_copy(struct stats_counter *counter)
+static inline struct stats_counter stats_counter_copy(const struct stats_counter *counter)
 {
   // TODO: locking for concurrent stats updates
   return *counter;
 }
 
-static inline unsigned stats_counter_ticks_passed(struct stats_counter *counter)
+static inline unsigned stats_counter_ticks_passed(const struct stats_counter *counter)
 {
   return (xTaskGetTickCount() - counter->tick);
 }
-static inline unsigned stats_counter_milliseconds_passed(struct stats_counter *counter)
+static inline unsigned stats_counter_milliseconds_passed(const struct stats_counter *counter)
 {
   return ((xTaskGetTickCount() - counter->tick) * portTICK_PERIOD_MS);
 }

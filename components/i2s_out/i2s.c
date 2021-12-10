4b14a902f3dd18938814ba6075cffd3a60f95367
Tero Marttila <terom@fixme.fi>
2021-12-10 02:34:06 +0200

    i2s_out: fix left/right 16-bit order

--- components/i2s_out/i2s.c
+++ components/i2s_out/i2s.c
@@ -22,15 +22,15 @@ void i2s_out_i2s_setup(struct i2s_out *i2s_out, struct i2s_out_options options)
 
   I2S0.conf.tx_slave_mod = 0; // generate output clock
   I2S0.conf.rx_slave_mod = 0;
-  I2S0.conf.right_first = 1; // XXX: ???
-  I2S0.conf.msb_right = 1; // XXX: ???
+  I2S0.conf.right_first = 0; // TX channels left -> right, matching FIFO byte order
+  I2S0.conf.msb_right = 1; // XXX: most significant bit first?
   // XXX: tx_msb_shift?
   // XXX: rx_msb_shift?
   I2S0.conf.bits_mod = 0; // XXX: probably (sample bits - 16, 0-15)
   I2S0.conf.clkm_div_num = options.clock.clkm_div;
   I2S0.conf.bck_div_num = options.clock.bck_div;
 
-  I2S0.fifo_conf.tx_fifo_mod = I2S_TX_FIFO_MODE_16BIT_FULL; // transmit 2 channels of 16-bit samples per 32-bit FIFO word
+  I2S0.fifo_conf.tx_fifo_mod = I2S_TX_FIFO_MODE_16BIT_FULL; // 16-bit left + 16-bit right
   I2S0.fifo_conf.rx_fifo_mod = I2S_RX_FIFO_MODE_16BIT_FULL; // unused
 
   I2S0.conf_chan.tx_chan_mod = I2S_TX_CHAN_MODE_DUAL; // transmit both channels

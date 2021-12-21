c5964cad19766a70b3bdc0edd6264a367708e15d
Tero Marttila <terom@fixme.fi>
2021-12-22 01:29:52 +0200

    main: fix pin_mutex crash when mutex held by deleted app_main() task

--- main/pin_mutex.c
+++ main/pin_mutex.c
@@ -5,7 +5,8 @@ SemaphoreHandle_t pin_mutex[PIN_MUTEX_MAX] = {};
 int init_pin_mutex()
 {
   for (enum pin_mutex m = 0; m < PIN_MUTEX_MAX; m++) {
-    if (!(pin_mutex[m] = xSemaphoreCreateMutex())) {
+    // use counting mutex initialized as available for `xSemaphoreTake()`.
+    if (!(pin_mutex[m] = xSemaphoreCreateCounting(1, 1))) {
       return -1;
     }
   }
--- main/pin_mutex.h
+++ main/pin_mutex.h
@@ -4,11 +4,16 @@
 #include <freertos/semphr.h>
 
 enum pin_mutex {
-  PIN_MUTEX_U0RXD,
+  PIN_MUTEX_U0RXD = 0,
 
   PIN_MUTEX_MAX,
 };
 
+/*
+ * Note: These are actually binary semaphores created using `xSemaphoreCreateCounting()`, because it is not necessarily
+ *       always the same task taking and giving the pin mutex. In particular, if the app_main() task takes the mutex,
+ *       then a later `xSemaphoreGive()` will crash (!)
+ */
 extern SemaphoreHandle_t pin_mutex[PIN_MUTEX_MAX];
 
 int init_pin_mutex();

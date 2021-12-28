a2d1d49b3c862c2d077ede28a939c690a8e9ebc4
Tero Marttila <terom@fixme.fi>
2021-12-28 16:06:12 +0200

    logging: esp-idf compat

--- components/logging/include/logging.h
+++ components/logging/include/logging.h
@@ -4,21 +4,21 @@
 #include "esp_log.h"
 
 /* Bypass stdio */
-#define LOG_BOOT_INFO(...)  do { esp_early_log_write(ESP_LOG_INFO, __func__, __VA_ARGS__); } while(0)
-#define LOG_BOOT_ERROR(...)  do { esp_early_log_write(ESP_LOG_ERROR, __func__, __VA_ARGS__); } while(0)
+#define LOG_BOOT_INFO(...)    ESP_EARLY_LOGI(__func__, __VA_ARGS__)
+#define LOG_BOOT_ERROR(...)   ESP_EARLY_LOGE(__func__, __VA_ARGS__)
 
 #ifdef DEBUG
-  #define LOG_DEBUG(...)  do { esp_log_write(ESP_LOG_DEBUG, __func__, __VA_ARGS__); } while(0)
-  #define LOG_BOOT_DEBUG(...)  do { esp_early_log_write(ESP_LOG_DEBUG, __func__, __VA_ARGS__); } while(0)
-  #define LOG_ISR_DEBUG(...)  do { esp_early_log_write(ESP_LOG_DEBUG, __func__, __VA_ARGS__); } while(0)
+  #define LOG_DEBUG(...)      ESP_EARLY_LOGD(__func__, __VA_ARGS__)
+  #define LOG_BOOT_DEBUG(...) ESP_EARLY_LOGD(__func__, __VA_ARGS__)
+  #define LOG_ISR_DEBUG(...)  ESP_EARLY_LOGD(__func__, __VA_ARGS__)
 #else
   #define LOG_DEBUG(...)
   #define LOG_BOOT_DEBUG(...)
   #define LOG_ISR_DEBUG(...)
 #endif
 
-#define LOG_INFO(...)   do { esp_log_write(ESP_LOG_INFO, __func__, __VA_ARGS__); } while(0)
-#define LOG_WARN(...)   do { esp_log_write(ESP_LOG_WARN, __func__, __VA_ARGS__); } while(0)
-#define LOG_ERROR(...)  do { esp_log_write(ESP_LOG_ERROR, __func__, __VA_ARGS__); } while(0)
+#define LOG_INFO(...)   ESP_LOG_LEVEL(ESP_LOG_INFO, __func__, __VA_ARGS__)
+#define LOG_WARN(...)   ESP_LOG_LEVEL(ESP_LOG_WARN, __func__, __VA_ARGS__)
+#define LOG_ERROR(...)  ESP_LOG_LEVEL(ESP_LOG_ERROR, __func__, __VA_ARGS__)
 
 #endif

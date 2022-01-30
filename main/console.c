6ea54714d6f687d071547cb879f3108014f9d86a
Tero Marttila <terom@fixme.fi>
2022-01-30 17:03:56 +0200

    main: console uart init/start

--- main/console.c
+++ main/console.c
@@ -5,6 +5,14 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include <sdkconfig.h>
+
+// uart
+#define UART_PORT (CONFIG_ESP_CONSOLE_UART_NUM)
+#define UART_BAUD_RATE (CONFIG_ESP_CONSOLE_UART_BAUDRATE)
+#define UART_RX_BUFFER_SIZE 256
+#define UART_TX_BUFFER_SIZE 1024
+
 // max line size
 #define CLI_BUF_SIZE 512
 #define CLI_MAX_ARGS 16
@@ -12,6 +20,7 @@
 #define CLI_TASK_STACK 2048 // bytes
 #define CLI_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
 
+struct uart *console_uart;
 static struct cli *console_cli;
 static xTaskHandle console_cli_task;
 
@@ -31,6 +40,21 @@ static const struct cmd console_cli_commands[] = {
   { "exit",   console_exit_cmd,     .describe = "Exit CLI" },
   {}
 };
+
+int init_console_uart()
+{
+  uart_port_t port = CONFIG_ESP_CONSOLE_UART_NUM;
+  int err;
+
+  LOG_INFO("port=%d", port);
+
+  if ((err = uart_new(&console_uart, port, UART_RX_BUFFER_SIZE, UART_TX_BUFFER_SIZE))) {
+    LOG_ERROR("uart_new");
+    return err;
+  }
+
+  return 0;
+}
 
 int init_console_cli()
 {
@@ -56,7 +80,63 @@ int init_console_cli()
 
 int init_console()
 {
-  return init_console_cli();
+  int err;
+
+  if ((err = init_console_uart())) {
+    LOG_ERROR("init_console_uart");
+    return err;
+  }
+
+  if ((err = init_console_cli())) {
+    LOG_ERROR("init_console_cli");
+    return err;
+  }
+
+  return 0;
+}
+
+int start_console_uart()
+{
+  struct uart_options options = {
+    .baud_rate    = UART_BAUD_RATE,
+    .data_bits    = UART_DATA_8_BITS,
+    .parity_bits  = UART_PARITY_DISABLE,
+    .stop_bits    = UART_STOP_BITS_1,
+/* TODO
+    .dev_mutex    = dev_mutex[DEV_MUTEX_UART0],
+    .pin_mutex    = pin_mutex[PIN_MUTEX_U0RXD],
+*/
+  };
+  int err;
+
+  LOG_INFO("baud_rate=%u, data_bits=%x parity=%x stop_bits=%x",
+    options.baud_rate,
+    options.data_bits,
+    options.parity_bits,
+    options.stop_bits
+  );
+
+  if ((err = uart_setup(console_uart, options))) {
+    LOG_ERROR("uart_setup");
+    return err;
+  }
+
+  LOG_INFO("started");
+
+  return 0;
+}
+
+int start_console_stdio()
+{
+  // TODO: hookup stdio -> uart
+
+  // unbuffered input
+  setvbuf(stdin, NULL, _IONBF, 0);
+
+  // line-buffered output
+  setvbuf(stdout, NULL, _IOLBF, 0);
+
+  return 0;
 }
 
 void console_cli_main(void *arg)
@@ -64,11 +144,19 @@ void console_cli_main(void *arg)
   struct cli *cli = arg;
   int err;
 
-  // unbuffered input
-  setvbuf(stdin, NULL, _IONBF, 0);
+  // NOTE: this may block waiting for other UART users to release
+  if ((err = start_console_uart())) {
+    LOG_ERROR("start_console_uart");
+    // TODO: alert?
+    goto exit;
+  }
 
-  // line-buffered output
-  setvbuf(stdout, NULL, _IOLBF, 0);
+  // NOTE: this may block waiting for other UART users to release
+  if ((err = start_console_stdio())) {
+    LOG_ERROR("start_console_stdio");
+    // TODO: alert?
+    goto exit;
+  }
 
   if ((err = cli_main(cli)) < 0) {
     LOG_ERROR("cli_main");
@@ -78,13 +166,21 @@ void console_cli_main(void *arg)
     LOG_INFO("cli exit");
   }
 
-  // exit
+  // TODO:
+  //stop_console_uart();
+
+exit:
   console_cli_task = NULL;
   vTaskDelete(NULL);
 }
 
 int start_console()
 {
+  if (console_cli_task) {
+    LOG_WARN("running: task=%p", console_cli_task);
+    return 1;
+  }
+
   // start task
   if (xTaskCreate(&console_cli_main, "console-cli", CLI_TASK_STACK, console_cli, CLI_TASK_PRIORITY, &console_cli_task) <= 0) {
     LOG_ERROR("xTaskCreate");
@@ -92,4 +188,9 @@ int start_console()
   }
 
   return 0;
+}
+
+bool is_console_running()
+{
+  return console_cli_task != NULL;
 }

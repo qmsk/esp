4a54097dae51b524252ce5a79a932d5d83692547
Tero Marttila <terom@fixme.fi>
2022-01-31 00:43:14 +0200

    logging: evaluate LOG_DEBUG(...) arguments even if DEBUG disabled

--- components/logging/include/logging.h
+++ components/logging/include/logging.h
@@ -3,20 +3,20 @@
 
 #include "esp_log.h"
 
+#ifdef DEBUG
+# define IF_DEBUG(x) do { if (1) x } while(0)
+#else
+# define IF_DEBUG(x) do { if (0) x } while(0)
+#endif
+
+#define LOG_ISR_DEBUG(...)  IF_DEBUG({ ESP_EARLY_LOGD(__func__, __VA_ARGS__); })
+
 /* Bypass stdio */
+#define LOG_BOOT_DEBUG(...)   IF_DEBUG({ ESP_EARLY_LOGD(__func__, __VA_ARGS__); })
 #define LOG_BOOT_INFO(...)    ESP_EARLY_LOGI(__func__, __VA_ARGS__)
 #define LOG_BOOT_ERROR(...)   ESP_EARLY_LOGE(__func__, __VA_ARGS__)
 
-#ifdef DEBUG
-  #define LOG_DEBUG(...)      ESP_LOG_LEVEL(ESP_LOG_DEBUG, __func__, __VA_ARGS__)
-  #define LOG_BOOT_DEBUG(...) ESP_EARLY_LOGD(__func__, __VA_ARGS__)
-  #define LOG_ISR_DEBUG(...)  ESP_EARLY_LOGD(__func__, __VA_ARGS__)
-#else
-  #define LOG_DEBUG(...)
-  #define LOG_BOOT_DEBUG(...)
-  #define LOG_ISR_DEBUG(...)
-#endif
-
+#define LOG_DEBUG(...)      IF_DEBUG({ ESP_LOG_LEVEL(ESP_LOG_DEBUG, __func__, __VA_ARGS__); })
 #define LOG_INFO(...)   ESP_LOG_LEVEL(ESP_LOG_INFO, __func__, __VA_ARGS__)
 #define LOG_WARN(...)   ESP_LOG_LEVEL(ESP_LOG_WARN, __func__, __VA_ARGS__)
 #define LOG_ERROR(...)  ESP_LOG_LEVEL(ESP_LOG_ERROR, __func__, __VA_ARGS__)

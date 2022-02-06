9e812ee6d4747afdad72e6b8850d0242a281d948
Tero Marttila <terom@fixme.fi>
2022-02-06 15:08:09 +0200

    main: system events

--- main/system.c
+++ main/system.c
@@ -16,9 +16,15 @@ int init_system()
     LOG_ERROR("esp_netif_init: %s", esp_err_to_name(err));
     return -1;
   }
+
   if ((err = esp_event_loop_create_default())) {
     LOG_ERROR("esp_event_loop_create_default: %s", esp_err_to_name(err));
     return -1;
+  }
+
+  if ((err = init_system_events())) {
+    LOG_ERROR("init_system_events");
+    return err;
   }
 
   return 0;
--- main/system.h
+++ main/system.h
@@ -3,5 +3,6 @@
 #include <cmd.h>
 
 int init_system();
+int init_system_events();
 
 extern const struct cmdtab system_cmdtab;

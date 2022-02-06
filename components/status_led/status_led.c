32a61794037b76ea94d42b0a996cf1c6a7a70bb0
Tero Marttila <terom@fixme.fi>
2022-02-06 20:09:46 +0200

    status_leds: also configure gpio pull-down for non-inverted pins

--- components/status_led/status_led.c
+++ components/status_led/status_led.c
@@ -261,6 +261,7 @@ int status_led_init_gpio(struct status_led *led, const struct status_led_options
       .pin_bit_mask = (1 << options.gpio),
       .mode         = GPIO_MODE_OUTPUT,
       .pull_up_en   = options.inverted ? 1 : 0,
+      .pull_down_en = options.inverted ? 0 : 1,
   };
   esp_err_t err;
 

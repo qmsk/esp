7b4d85c0680fa5902ed44adfca729955b757da96
Tero Marttila <terom@fixme.fi>
2022-02-07 20:06:51 +0200

    system: #define SYSTEM_* types

--- components/system/include/system_interfaces.h
+++ components/system/include/system_interfaces.h
@@ -4,50 +4,52 @@
 
 #if CONFIG_IDF_TARGET_ESP8266
 # include <tcpip_adapter.h>
-#elif CONFIG_IDF_TARGET_ESP32
-# include <esp_netif.h>
-#endif
 
-#include <stdbool.h>
-
-#if CONFIG_IDF_TARGET_ESP8266
+# define SYSTEM_INTERFACE_TYPE tcpip_adapter_if_t
+# define SYSTEM_DHCP_STATUS_TYPE tcpip_adapter_dhcp_status_t
+# define SYSTEM_IPV4_ADDR_TYPE ip4_addr_t
+# define SYSTEM_IPV4_ADDR_BYTES(x) ip4_addr1(x), ip4_addr2(x), ip4_addr3(x), ip4_addr4(x)
+# define SYSTEM_IPV4_ADDR_BYTE1(x) ip4_addr1(x)
+# define SYSTEM_IPV4_ADDR_BYTE2(x) ip4_addr2(x)
+# define SYSTEM_IPV4_ADDR_BYTE3(x) ip4_addr3(x)
+# define SYSTEM_IPV4_ADDR_BYTE4(x) ip4_addr4(x)
+# define SYSTEM_IP_ADDR_TYPE ip_addr_t
 
   const char *tcpip_adapter_if_str(tcpip_adapter_if_t tcpip_if);
   const char *tcpip_adapter_dhcp_status_str(tcpip_adapter_dhcp_status_t status);
 
-  struct system_interface_info {
-    tcpip_adapter_if_t interface;
-
-    const char *hostname;
-
-    tcpip_adapter_dhcp_status_t dhcps_status, dhcpc_status;
-
-    ip4_addr_t ipv4_address, ipv4_netmask, ipv4_gateway;
-    ip4_addr_t ipv4_network;
-    unsigned ipv4_prefixlen;
-
-    ip_addr_t dns_main, dns_backup, dns_fallback;
-  };
-
-
 #elif CONFIG_IDF_TARGET_ESP32
+# include <esp_netif.h>
+
+
+# define SYSTEM_INTERFACE_TYPE esp_netif_t *
+# define SYSTEM_DHCP_STATUS_TYPE esp_netif_dhcp_status_t
+# define SYSTEM_IPV4_ADDR_TYPE esp_ip4_addr_t
+# define SYSTEM_IPV4_ADDR_BYTES(x) esp_ip4_addr1(x), esp_ip4_addr2(x), esp_ip4_addr3(x), esp_ip4_addr4(x)
+# define SYSTEM_IPV4_ADDR_BYTE1(x) esp_ip4_addr1(x)
+# define SYSTEM_IPV4_ADDR_BYTE2(x) esp_ip4_addr2(x)
+# define SYSTEM_IPV4_ADDR_BYTE3(x) esp_ip4_addr3(x)
+# define SYSTEM_IPV4_ADDR_BYTE4(x) esp_ip4_addr4(x)
+# define SYSTEM_IP_ADDR_TYPE esp_ip_addr_t
 
   const char *esp_netif_dhcp_status_str(esp_netif_dhcp_status_t status);
+#endif
 
-  struct system_interface_info {
-    esp_netif_t *interface;
+struct system_interface_info {
+  SYSTEM_INTERFACE_TYPE interface;
 
-    const char *hostname;
+  const char *hostname;
 
-    esp_netif_dhcp_status_t dhcps_status, dhcpc_status;
+  SYSTEM_DHCP_STATUS_TYPE dhcps_status, dhcpc_status;
 
-    esp_ip4_addr_t ipv4_address, ipv4_netmask, ipv4_gateway;
-    esp_ip4_addr_t ipv4_network;
-    unsigned ipv4_prefixlen;
+  SYSTEM_IPV4_ADDR_TYPE ipv4_address, ipv4_netmask, ipv4_gateway;
+  SYSTEM_IPV4_ADDR_TYPE ipv4_network;
+  unsigned ipv4_prefixlen;
 
-    esp_ip_addr_t dns_main, dns_backup, dns_fallback;
-  };
+  SYSTEM_IP_ADDR_TYPE dns_main, dns_backup, dns_fallback;
+};
 
+#if CONFIG_IDF_TARGET_ESP32
   struct system_interface_client {
     uint8_t mac[6];
     esp_ip4_addr_t ipv4;
@@ -64,6 +66,11 @@
   {
     return tcpip_adapter_dhcp_status_str(status);
   }
+
+  /*
+   * Returns <0 on error, 0 if valid, 1 if not configured/enabled/available.
+   */
+  int system_interface_info(struct system_interface_info *info, tcpip_adapter_if_t interface);
 #elif CONFIG_IDF_TARGET_ESP32
   static inline const char *system_interface_str(const struct system_interface_info *info)
   {
@@ -74,17 +81,17 @@
   {
     return esp_netif_dhcp_status_str(status);
   }
-#endif
 
-
-/*
- * Returns <0 on error, 0 if valid, 1 if not configured/enabled/available.
- */
-#if CONFIG_IDF_TARGET_ESP8266
-  int system_interface_info(struct system_interface_info *info, tcpip_adapter_if_t interface);
-#elif CONFIG_IDF_TARGET_ESP32
+  /*
+   * Returns <0 on error, 0 if valid, 1 if not configured/enabled/available.
+   */
   int system_interface_info(struct system_interface_info *info, esp_netif_t *interface);
 
   int system_interface_walk(int (*func)(const struct system_interface_info *info, void *ctx), void *ctx);
   int system_interface_clients_walk(int (*func)(const struct system_interface_client *client, void *ctx), void *ctx);
+#endif
+
+
+#if CONFIG_IDF_TARGET_ESP8266
+#elif CONFIG_IDF_TARGET_ESP32
 #endif

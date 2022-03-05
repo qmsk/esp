b6006dbbe57d0e50053a6b6545682d8f4d8145b1
Tero Marttila <>
2022-03-05 19:40:04 +0200

    main: implement esp8266/esp32 esp_log stderr redirect

--- main/log.c
+++ main/log.c
@@ -5,14 +5,23 @@
 
 #include <stdio.h>
 
+#include <sdkconfig.h>
+
 #define STDIO_LOG_SIZE 1024
 
 struct stdio_log *log_stderr;
 
-static int vprintf_stderr(const char * fmt, va_list args)
-{
-  return vfprintf(stderr, fmt, args);
-}
+#if CONFIG_IDF_TARGET_ESP8266
+  static int putchar_stderr(int c)
+  {
+    return fputc(c, stderr);
+  }
+#elif CONFIG_IDF_TARGET_ESP32
+  static int vprintf_stderr(const char * fmt, va_list args)
+  {
+    return vfprintf(stderr, fmt, args);
+  }
+#endif
 
 int init_log()
 {
@@ -31,8 +40,13 @@ int init_log()
   // unbuffered logging via stderr
   setvbuf(stderr, NULL, _IONBF, 0);
 
+#if CONFIG_IDF_TARGET_ESP8266
+  // redirect esp_log via stderr
+  esp_log_set_putchar(putchar_stderr);
+#elif CONFIG_IDF_TARGET_ESP32
   // redirect esp_log via stderr
   esp_log_set_vprintf(vprintf_stderr);
+#endif
 
   return 0;
 }

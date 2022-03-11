c1c9219e23c0cf5110b9f5344287b9127d8ba27a
Tero Marttila <>
2022-03-11 19:07:38 +0200

    main status-leds: cmd MODE

--- main/status_leds_cmd.c
+++ main/status_leds_cmd.c
@@ -1,33 +1,45 @@
 #include "status_leds.h"
 #include "status_leds_set.h"
 
+#include <string.h>
+
 #include <sdkconfig.h>
 
+static int parse_status_led_mode(const char *str)
+{
+  if (!strcasecmp(str, "off")) {
+    return STATUS_LED_OFF;
+  } else if (!strcasecmp(str, "on")) {
+    return STATUS_LED_ON;
+  } else if (!strcasecmp(str, "slow")) {
+    return STATUS_LED_SLOW;
+  } else if (!strcasecmp(str, "fast")) {
+    return STATUS_LED_FAST;
+  } else if (!strcasecmp(str, "flash")) {
+    return STATUS_LED_FLASH;
+  } else if (!strcasecmp(str, "pulse")) {
+    return STATUS_LED_PULSE;
+  } else {
+    return -1;
+  }
+}
+
 #if CONFIG_STATUS_LEDS_USER_ENABLED
-int status_leds_off_cmd(int argc, char **argv, void *ctx)
+int status_leds_user_cmd(int argc, char **argv, void *ctx)
 {
-  set_user_led(STATUS_LED_OFF);
+  enum status_led_mode mode = STATUS_LED_ON;
+  const char *arg = NULL;
+  int err;
 
-  return 0;
-}
+  if (argc <= 1) {
 
-int status_leds_on_cmd(int argc, char **argv, void *ctx)
-{
-  set_user_led(STATUS_LED_ON);
+  } else if ((err = cmd_arg_str(argc, argv, 1, &arg))) {
+    return err;
+  } else if ((mode = parse_status_led_mode(arg)) < 0) {
+    return -CMD_ERR_ARGV;
+  }
 
-  return 0;
-}
-
-int status_leds_slow_cmd(int argc, char **argv, void *ctx)
-{
-  set_user_led(STATUS_LED_SLOW);
-
-  return 0;
-}
-
-int status_leds_fast_cmd(int argc, char **argv, void *ctx)
-{
-  set_user_led(STATUS_LED_FAST);
+  set_user_led(mode);
 
   return 0;
 }
@@ -36,7 +48,19 @@ int status_leds_fast_cmd(int argc, char **argv, void *ctx)
 #if CONFIG_STATUS_LEDS_FLASH_ENABLED
 int status_leds_flash_cmd(int argc, char **argv, void *ctx)
 {
-  set_flash_led(STATUS_LED_FLASH);
+  enum status_led_mode mode = STATUS_LED_FLASH;
+  const char *arg = NULL;
+  int err;
+
+  if (argc <= 1) {
+
+  } else if ((err = cmd_arg_str(argc, argv, 1, &arg))) {
+    return err;
+  } else if ((mode = parse_status_led_mode(arg)) < 0) {
+    return -CMD_ERR_ARGV;
+  }
+
+  set_flash_led(mode);
 
   return 0;
 }
@@ -45,14 +69,19 @@ int status_leds_flash_cmd(int argc, char **argv, void *ctx)
 #if CONFIG_STATUS_LEDS_ALERT_ENABLED
 int status_leds_alert_cmd(int argc, char **argv, void *ctx)
 {
-  set_alert_led(STATUS_LED_ON);
+  enum status_led_mode mode = STATUS_LED_ON;
+  const char *arg = NULL;
+  int err;
 
-  return 0;
-}
+  if (argc <= 1) {
 
-int status_leds_clear_cmd(int argc, char **argv, void *ctx)
-{
-  set_alert_led(STATUS_LED_OFF);
+  } else if ((err = cmd_arg_str(argc, argv, 1, &arg))) {
+    return err;
+  } else if ((mode = parse_status_led_mode(arg)) < 0) {
+    return -CMD_ERR_ARGV;
+  }
+
+  set_alert_led(mode);
 
   return 0;
 }
@@ -60,17 +89,13 @@ int status_leds_clear_cmd(int argc, char **argv, void *ctx)
 
 const struct cmd status_leds_commands[] = {
 #if CONFIG_STATUS_LEDS_USER_ENABLED
-  { "off",    status_leds_off_cmd,      .describe = "Turn off USER LED" },
-  { "on",     status_leds_on_cmd,       .describe = "Turn on USER LED" },
-  { "slow",   status_leds_slow_cmd,     .describe = "Blink USER LED slowly" },
-  { "fast",   status_leds_fast_cmd,     .describe = "Blink USER LED fast" },
+  { "user",   status_leds_user_cmd,     .usage = "[MODE]",  .describe = "Set USER LED" },
 #endif
 #if CONFIG_STATUS_LEDS_FLASH_ENABLED
-  { "flash",  status_leds_flash_cmd,    .describe = "Blink FLASH LED once" },
+  { "flash",  status_leds_flash_cmd,    .usage = "[MODE]",  .describe = "Set FLASH LED" },
 #endif
 #if CONFIG_STATUS_LEDS_ALERT_ENABLED
-  { "alert",  status_leds_alert_cmd,    .describe = "Turn on ALERT LED" },
-  { "clear",  status_leds_clear_cmd,    .describe = "Turn off ALERT LED" },
+  { "alert",  status_leds_alert_cmd,    .usage = "[MODE]",  .describe = "Set ALERT LED" },
 #endif
   {}
 };

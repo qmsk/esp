2cd6b4e2e8a048e426e215d80d56c793ab9ba5f4
Tero Marttila <>
2022-03-13 19:48:02 +0200

    leds: fix ws2812b CONFIG_LEDS_I2S_ENABLED

--- components/leds/interfaces/ws2812b.h
+++ components/leds/interfaces/ws2812b.h
@@ -7,7 +7,7 @@
   int leds_tx_uart_ws2812b(const struct leds_options *options, union ws2812b_pixel *pixels, unsigned count);
 #endif
 
-#if CONFIG_LEDS_UART_ENABLED
+#if CONFIG_LEDS_I2S_ENABLED
   /* interfaces/i2s/ws2812b.c */
   int leds_tx_i2s_ws2812b(const struct leds_options *options, union ws2812b_pixel *pixels, unsigned count);
 #endif

95167a9a0912331ca22414e99b27bed9e18fa289
Tero Marttila <terom@fixme.fi>
2022-03-23 20:45:20 +0200

    gpio_out: remove broken/unused internal gpio_pins_set/clear()

--- components/gpio_out/esp8266/gpio_pins.h
+++ components/gpio_out/esp8266/gpio_pins.h
@@ -7,13 +7,3 @@ static inline void gpio_pins_out(enum gpio_out_pins pins, enum gpio_out_pins lev
   GPIO.out_w1ts = (pins & levels);
   GPIO.out_w1tc = (pins & ~levels);
 }
-
-static inline void gpio_pins_set(enum gpio_out_pins pins, enum gpio_out_pins level)
-{
-  GPIO.out_w1ts = pins;
-}
-
-static inline void gpio_pins_clear(enum gpio_out_pins pins, enum gpio_out_pins level)
-{
-  GPIO.out_w1tc = pins;
-}

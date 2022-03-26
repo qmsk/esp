eacc1e073527a3800ee23d407095294eb69693b0
Tero Marttila <terom@fixme.fi>
2022-03-26 11:28:59 +0200

    main console: use configtab .default_value

--- main/console_config.c
+++ main/console_config.c
@@ -1,15 +1,12 @@
 #include "console.h"
 #include "console_config.h"
 
-struct console_config console_config = {
-  .enabled  = CONSOLE_CLI_ENABLED,
-  .timeout  = CONSOLE_CLI_TIMEOUT,
-};
+struct console_config console_config = { };
 
 const struct configtab console_configtab[] = {
   { CONFIG_TYPE_BOOL, "enabled",
     .description = "Start UART console at boot",
-    .bool_type = { .value = &console_config.enabled },
+    .bool_type = { .value = &console_config.enabled, .default_value = CONSOLE_CLI_ENABLED },
   },
   { CONFIG_TYPE_UINT16, "timeout",
     .description = "Wait [ms] timeout to open UART console",

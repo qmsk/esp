f446089b0cd7ae8459a2394558bc6b23a786d76b
Tero Marttila <terom@fixme.fi>
2022-04-30 22:05:16 +0300

    leds: use new i2s tx for sk9822

--- components/leds/interfaces/sk9822.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../interface.h"
-#include "../protocols/sk9822.h"
-
-#if CONFIG_LEDS_I2S_ENABLED
-  /* interfaces/i2s/sk9822.c */
-  int leds_tx_i2s_sk9822(const struct leds_interface_i2s_options *options, union sk9822_pixel *pixels, unsigned count, struct leds_limit limit);
-#endif
--- components/leds/protocols/sk9822.c
+++ components/leds/protocols/sk9822.c
@@ -1,10 +1,19 @@
 #include "sk9822.h"
 #include "../leds.h"
-#include "../interfaces/sk9822.h"
+#include "../interfaces/i2s.h"
 
 #include <logging.h>
 
 #include <stdlib.h>
+
+static void leds_protocol_sk9822_i2s_out(uint32_t buf[1], void *data, unsigned index, struct leds_limit limit)
+{
+  union sk9822_pixel *pixels = data;
+  uint32_t xbgr = sk9822_pixel_limit(pixels[index], limit).xbgr;
+
+  // 32-bit little-endian
+  buf[0] = xbgr;
+}
 
 int leds_protocol_sk9822_init(struct leds_protocol_sk9822 *protocol, union leds_interface_state *interface, const struct leds_options *options)
 {
@@ -26,7 +35,14 @@ int leds_protocol_sk9822_tx(struct leds_protocol_sk9822 *protocol, union leds_in
 
   #if CONFIG_LEDS_I2S_ENABLED
     case LEDS_INTERFACE_I2S:
-      return leds_tx_i2s_sk9822(&options->i2s, protocol->pixels, protocol->count, limit);
+      return leds_interface_i2s_tx(&options->i2s, LEDS_INTERFACE_I2S_MODE_32BIT_BCK, (struct leds_interface_i2s_tx) {
+        .data   = protocol->pixels,
+        .count  = protocol->count,
+        .limit  = limit,
+
+        .start_frame.i2s_mode_32bit = SK9822_START_FRAME_UINT32,
+        .func.i2s_mode_32bit        = leds_protocol_sk9822_i2s_out,
+      });
   #endif
 
     default:

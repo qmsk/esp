ad9c3d7ce2dd004e9fccd841cec461283fb9d601
Tero Marttila <terom@fixme.fi>
2022-05-04 23:07:03 +0300

    logging: fix LOG_DEBUG, add (experimental) LOG_FATAL

--- components/logging/include/logging.h
+++ components/logging/include/logging.h
@@ -18,12 +18,17 @@
 #define LOG_BOOT_INFO(...)    ESP_EARLY_LOGI(__func__, __VA_ARGS__)
 #define LOG_BOOT_ERROR(...)   ESP_EARLY_LOGE(__func__, __VA_ARGS__)
 
-#define LOG_DEBUG(...)      IF_DEBUG({ ESP_LOG_LEVEL(ESP_LOG_DEBUG, __func__, __VA_ARGS__); })
+// XXX: CONFIG_LOG_DEFAULT_LEVEL defaults to skip ESP_LOG_DEBUG, and raising will include ALL debug output by default - not possible to override this per-call
+#define LOG_DEBUG(...)      IF_DEBUG({ ESP_LOG_LEVEL(ESP_LOG_INFO, __func__, __VA_ARGS__); })
 #define LOG_INFO(...)   ESP_LOG_LEVEL(ESP_LOG_INFO, __func__, __VA_ARGS__)
 #define LOG_WARN(...)   ESP_LOG_LEVEL(ESP_LOG_WARN, __func__, __VA_ARGS__)
 #define LOG_ERROR(...)  ESP_LOG_LEVEL(ESP_LOG_ERROR, __func__, __VA_ARGS__)
 
-#define LOG_DEBUG_BUFFER(buf, len)      IF_DEBUG({ ESP_LOG_BUFFER_HEX_LEVEL(__func__, buf, len, ESP_LOG_DEBUG); })
+// TODO: fprint(stderr) + fflush(stderr) + fsync(stderr) + abort()?
+#define LOG_FATAL(...)  do { ESP_LOG_LEVEL(ESP_LOG_ERROR, __func__, __VA_ARGS__); abort(); } while(0)
+
+// XXX: CONFIG_LOG_DEFAULT_LEVEL defaults to skip ESP_LOG_DEBUG, and raising will include ALL debug output by default - not possible to override this per-call
+#define LOG_DEBUG_BUFFER(buf, len)      IF_DEBUG({ ESP_LOG_BUFFER_HEX_LEVEL(__func__, buf, len, ESP_LOG_INFO); })
 #define LOG_INFO_BUFFER(buf, len)      ESP_LOG_BUFFER_HEX_LEVEL(__func__, buf, len, ESP_LOG_INFO);
 
 #endif

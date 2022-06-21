a479a980cbe75468a490fca3caf65e8af78a6f81
Tero Marttila <terom@fixme.fi>
2022-06-21 23:03:42 +0300

    web: use vue-style-loader in dev, MiniCssExtractPlugin.loader in prod/build

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,47 +3,48 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js', // full bundle with compiler
+module.exports = (env, argv) => {
+  return {
+    entry: './src/index.js',
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js', // full bundle with compiler
+      },
+      extensions: ['.js', '.vue'],
     },
-    extensions: ['.js', '.vue'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            argv.mode !== 'production' ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader'
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-  },
-  devServer: {
-    contentBase: './dist',
-    proxy: {
-      '/api': process.env.API_URL,
-      '/config.ini': process.env.API_URL,
-    }
-  },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+    },
+    devServer: {
+      contentBase: './dist',
+      proxy: {
+        '/api': process.env.API_URL,
+        '/config.ini': process.env.API_URL,
+      }
+    },
+  };
 };

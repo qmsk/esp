84b540a7536f6bd76175e63f9f311e4d01fcde72
Tero Marttila <terom@fixme.fi>
2022-06-26 23:41:12 +0300

    gpio: fix !GPIO_I2C_ENABLED compile

--- components/gpio/gpio.c
+++ components/gpio/gpio.c
@@ -14,9 +14,11 @@ int gpio_setup(const struct gpio_options *options)
     case GPIO_TYPE_HOST:
       return gpio_host_setup(options);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_setup(options);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);
@@ -29,9 +31,11 @@ int gpio_in_setup(const struct gpio_options *options, gpio_pins_t pins)
     case GPIO_TYPE_HOST:
       return gpio_host_setup_input(options, pins);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_setup_input(options, pins);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);
@@ -44,9 +48,11 @@ int gpio_in_get(const struct gpio_options *options, gpio_pins_t *pins)
     case GPIO_TYPE_HOST:
       return gpio_host_get(options, pins);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_get(options, pins);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);
@@ -59,9 +65,11 @@ int gpio_out_setup(const struct gpio_options *options, gpio_pins_t pins)
     case GPIO_TYPE_HOST:
       return gpio_host_setup_output(options, pins);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_setup_output(options, pins);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);
@@ -74,9 +82,11 @@ int gpio_out_clear(const struct gpio_options *options)
     case GPIO_TYPE_HOST:
       return gpio_host_clear(options);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_clear(options);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);
@@ -89,9 +99,11 @@ int gpio_out_set(const struct gpio_options *options, gpio_pins_t pins)
     case GPIO_TYPE_HOST:
       return gpio_host_set(options, pins);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_set(options, pins);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);
@@ -104,9 +116,11 @@ int gpio_out_set_all(const struct gpio_options *options)
     case GPIO_TYPE_HOST:
       return gpio_host_set_all(options);
 
+  #if GPIO_I2C_ENABLED
     case GPIO_TYPE_I2C_PCA9534:
     case GPIO_TYPE_I2C_PCA9554:
       return gpio_i2c_pc54xx_set_all(options);
+  #endif
 
     default:
       LOG_FATAL("unsupported type=%d", options->type);

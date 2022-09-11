f303f28c883d9ba610483b48216257a05450b724
Tero Marttila <terom@fixme.fi>
2022-09-11 12:13:17 +0300

    uart: indent header ifdefs

--- components/uart/include/uart.h
+++ components/uart/include/uart.h
@@ -9,59 +9,58 @@
 
 #if CONFIG_IDF_TARGET_ESP8266
 
-typedef int uart_port_t;
+  typedef int uart_port_t;
 
-#define UART_0    (0) // GPIO1 TX, GPIO3 RX
-#define UART_1    (1) // GPIO2 TX
+  #define UART_0    (0) // GPIO1 TX, GPIO3 RX
+  #define UART_1    (1) // GPIO2 TX
 
-// number of physical UART ports
-#define UART_PORT_MAX   (2)
-#define UART_PORT_MASK  0x0ff
+  // number of physical UART ports
+  #define UART_PORT_MAX   (2)
+  #define UART_PORT_MASK  0x0ff
 
-// flag bits
-#define UART_SWAP_BIT   0x100   // swap RTS/CST AND RX/TX pins
-#define UART_TXDBK_BIT  0x200   // use alternate TX pin
-#define UART_RXONLY_BIT 0x400   // skip TX pin
-#define UART_TXONLY_BIT 0x800   // skip RX pin
+  // flag bits
+  #define UART_SWAP_BIT   0x100   // swap RTS/CST AND RX/TX pins
+  #define UART_TXDBK_BIT  0x200   // use alternate TX pin
+  #define UART_RXONLY_BIT 0x400   // skip TX pin
+  #define UART_TXONLY_BIT 0x800   // skip RX pin
 
-#define UART_0_SWAP         (UART_0 | UART_SWAP_BIT)                    // GPIO15 TX, GPIO13 RX
-#define UART_0_SWAP_RXONLY  (UART_0 | UART_SWAP_BIT | UART_RXONLY_BIT)  // GPIO13 RX
-#define UART_0_SWAP_TXONLY  (UART_0 | UART_SWAP_BIT | UART_TXONLY_BIT)  // GPIO15 TX
-#define UART_0_TXDBK        (UART_0 | UART_TXDBK_BIT)                   // GPIO1+GPIO2 TX, GPIO3 RX
+  #define UART_0_SWAP         (UART_0 | UART_SWAP_BIT)                    // GPIO15 TX, GPIO13 RX
+  #define UART_0_SWAP_RXONLY  (UART_0 | UART_SWAP_BIT | UART_RXONLY_BIT)  // GPIO13 RX
+  #define UART_0_SWAP_TXONLY  (UART_0 | UART_SWAP_BIT | UART_TXONLY_BIT)  // GPIO15 TX
+  #define UART_0_TXDBK        (UART_0 | UART_TXDBK_BIT)                   // GPIO1+GPIO2 TX, GPIO3 RX
 
-typedef enum {
-  UART_DATA_5_BITS = 0,
-  UART_DATA_6_BITS = 1,
-  UART_DATA_7_BITS = 2,
-  UART_DATA_8_BITS = 3,
-} uart_word_length_t;
+  typedef enum {
+    UART_DATA_5_BITS = 0,
+    UART_DATA_6_BITS = 1,
+    UART_DATA_7_BITS = 2,
+    UART_DATA_8_BITS = 3,
+  } uart_word_length_t;
 
-typedef enum {
-  UART_PARITY_DISABLE = 0x0,
-  UART_PARITY_EVEN    = 0x2,
-  UART_PARITY_ODD     = 0x3,
-} uart_parity_t;
+  typedef enum {
+    UART_PARITY_DISABLE = 0x0,
+    UART_PARITY_EVEN    = 0x2,
+    UART_PARITY_ODD     = 0x3,
+  } uart_parity_t;
 
-typedef enum {
-  UART_STOP_BITS_1   = 0x1,
-  UART_STOP_BITS_1_5 = 0x2,
-  UART_STOP_BITS_2   = 0x3,
-} uart_stop_bits_t;
+  typedef enum {
+    UART_STOP_BITS_1   = 0x1,
+    UART_STOP_BITS_1_5 = 0x2,
+    UART_STOP_BITS_2   = 0x3,
+  } uart_stop_bits_t;
 
 #elif CONFIG_IDF_TARGET_ESP32
-# include <hal/uart_types.h>
+  #include <hal/uart_types.h>
 
-// uart_port_t values
-#define UART_0  (0)     // GPIO1 TX, GPIO3 RX
-#define UART_1  (1)     // GPIO10 TX, GPIO9 RX
-#define UART_2  (2)     // GPIO17 TX, GPIO16 RX
+  // uart_port_t values
+  #define UART_0  (0)     // GPIO1 TX, GPIO3 RX
+  #define UART_1  (1)     // GPIO10 TX, GPIO9 RX
+  #define UART_2  (2)     // GPIO17 TX, GPIO16 RX
 
-// number of physical UART ports
-#define UART_PORT_MAX 3
-#define UART_PORT_MASK  0x0ff
-
+  // number of physical UART ports
+  #define UART_PORT_MAX 3
+  #define UART_PORT_MASK  0x0ff
 #else
-# error Unsupported target
+  #error Unsupported target
 #endif
 
 typedef enum {

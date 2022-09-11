a65c2f3dcb65908eb95eb45d52dd9dd98de79b71
Tero Marttila <terom@fixme.fi>
2022-09-11 11:58:41 +0300

    i2s_out: configure esp32 gpio/rtc pull-down for i2s clock/data pins

--- components/i2s_out/esp32/pin.c
+++ components/i2s_out/esp32/pin.c
@@ -2,7 +2,9 @@
 
 #include <esp_rom_gpio.h>
 #include <hal/gpio_ll.h>
+#include <hal/rtc_io_ll.h>
 #include <soc/gpio_periph.h>
+#include <soc/rtc_io_periph.h>
 
 #include <logging.h>
 
@@ -19,6 +21,37 @@ static const uint8_t i2s_serial_data_out_sig[I2S_PORT_MAX] = {
 static const uint8_t i2s_parallel8_data_out_sig[I2S_PORT_MAX] = {
   [I2S_PORT_1]  = I2S1O_DATA_OUT0_IDX, // special 8-bit mode on I2S1 only
 };
+
+static void setup_rtc_pin(gpio_num_t gpio)
+{
+#if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
+  int rtc_ionum;
+
+  if ((rtc_ionum = rtc_io_num_map[gpio]) < 0) {
+    return;
+  }
+
+  rtcio_ll_pullup_disable(rtc_ionum);
+  rtcio_ll_pulldown_enable(rtc_ionum);
+
+  // configure RTC IOMUX as digital GPIO
+  rtcio_ll_function_select(rtc_ionum, RTCIO_FUNC_DIGITAL);
+#endif
+}
+
+static void setup_gpio_pin(gpio_num_t gpio)
+{
+  gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[gpio], PIN_FUNC_GPIO);
+  gpio_ll_input_disable(&GPIO, gpio);
+  gpio_ll_output_enable(&GPIO, gpio);
+  gpio_ll_pullup_dis(&GPIO, gpio);
+  gpio_ll_pulldown_en(&GPIO, gpio);
+}
+
+static void disable_gpio_pin(gpio_num_t gpio)
+{
+  gpio_ll_output_disable(&GPIO, gpio);
+}
 
 int i2s_out_pin_init(struct i2s_out *i2s_out)
 {
@@ -95,9 +128,8 @@ int i2s_out_pin_setup(struct i2s_out *i2s_out, const struct i2s_out_options *opt
       if (options->bck_gpio > 0) {
         i2s_out->bck_gpios[0] = options->bck_gpio;
 
-        gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[options->bck_gpio], PIN_FUNC_GPIO);
-        gpio_ll_input_disable(&GPIO, options->bck_gpio);
-        gpio_ll_output_enable(&GPIO, options->bck_gpio);
+        setup_gpio_pin(options->bck_gpio);
+        setup_rtc_pin(options->bck_gpio);
 
         esp_rom_gpio_connect_out_signal(options->bck_gpio, i2s_bck_out_sig[i2s_out->port], options->bck_inv, false);
       }
@@ -105,9 +137,8 @@ int i2s_out_pin_setup(struct i2s_out *i2s_out, const struct i2s_out_options *opt
       if (options->data_gpio > 0) {
         i2s_out->data_gpios[0] = options->data_gpio;
 
-        gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[options->data_gpio], PIN_FUNC_GPIO);
-        gpio_ll_input_disable(&GPIO, options->data_gpio);
-        gpio_ll_output_enable(&GPIO, options->data_gpio);
+        setup_gpio_pin(options->data_gpio);
+        setup_rtc_pin(options->data_gpio);
 
         esp_rom_gpio_connect_out_signal(options->data_gpio, i2s_serial_data_out_sig[i2s_out->port], false, false);
       }
@@ -115,9 +146,8 @@ int i2s_out_pin_setup(struct i2s_out *i2s_out, const struct i2s_out_options *opt
       if (options->inv_data_gpio > 0) {
         i2s_out->inv_data_gpios[0] = options->inv_data_gpio;
 
-        gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[options->inv_data_gpio], PIN_FUNC_GPIO);
-        gpio_ll_input_disable(&GPIO, options->inv_data_gpio);
-        gpio_ll_output_enable(&GPIO, options->inv_data_gpio);
+        setup_gpio_pin(options->inv_data_gpio);
+        setup_rtc_pin(options->inv_data_gpio);
 
         // invert gpio output pin
         esp_rom_gpio_connect_out_signal(options->inv_data_gpio, i2s_serial_data_out_sig[i2s_out->port], true, false);
@@ -130,9 +160,8 @@ int i2s_out_pin_setup(struct i2s_out *i2s_out, const struct i2s_out_options *opt
         if (options->bck_gpios[i] > 0) {
           i2s_out->bck_gpios[i] = options->bck_gpios[i];
 
-          gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[options->bck_gpios[i]], PIN_FUNC_GPIO);
-          gpio_ll_input_disable(&GPIO, options->bck_gpios[i]);
-          gpio_ll_output_enable(&GPIO, options->bck_gpios[i]);
+          setup_gpio_pin(options->bck_gpios[i]);
+          setup_rtc_pin(options->bck_gpios[i]);
 
           esp_rom_gpio_connect_out_signal(options->bck_gpios[i], i2s_bck_out_sig[i2s_out->port], options->bck_inv, false);
         }
@@ -140,9 +169,8 @@ int i2s_out_pin_setup(struct i2s_out *i2s_out, const struct i2s_out_options *opt
         if (options->data_gpios[i] > 0) {
           i2s_out->data_gpios[i] = options->data_gpios[i];
 
-          gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[options->data_gpios[i]], PIN_FUNC_GPIO);
-          gpio_ll_input_disable(&GPIO, options->data_gpios[i]);
-          gpio_ll_output_enable(&GPIO, options->data_gpios[i]);
+          setup_gpio_pin(options->data_gpios[i]);
+          setup_rtc_pin(options->data_gpios[i]);
 
           // data[0] is mapped to the most significant bit, which is OUT7
           esp_rom_gpio_connect_out_signal(options->data_gpios[i], i2s_parallel8_data_out_sig[i2s_out->port] + 8 - i - 1, false, false);
@@ -151,9 +179,8 @@ int i2s_out_pin_setup(struct i2s_out *i2s_out, const struct i2s_out_options *opt
         if (options->inv_data_gpios[i] > 0) {
           i2s_out->inv_data_gpios[i] = options->inv_data_gpios[i];
 
-          gpio_ll_iomux_func_sel(GPIO_PIN_MUX_REG[options->inv_data_gpios[i]], PIN_FUNC_GPIO);
-          gpio_ll_input_disable(&GPIO, options->inv_data_gpios[i]);
-          gpio_ll_output_enable(&GPIO, options->inv_data_gpios[i]);
+          setup_gpio_pin(options->inv_data_gpios[i]);
+          setup_rtc_pin(options->inv_data_gpios[i]);
 
           // data[0] is mapped to the most significant bit, which is OUT7
           esp_rom_gpio_connect_out_signal(options->inv_data_gpios[i], i2s_parallel8_data_out_sig[i2s_out->port] + 8 - i - 1, true, false);
@@ -176,19 +203,19 @@ void i2s_out_pin_teardown(struct i2s_out *i2s_out)
 
   for (int i = 0; i < I2S_OUT_PARALLEL_SIZE; i++) {
     if (i2s_out->bck_gpios[i] >= 0) {
-      gpio_ll_output_disable(&GPIO, i2s_out->bck_gpios[i]);
+      disable_gpio_pin(i2s_out->bck_gpios[i]);
 
       i2s_out->bck_gpios[i] = -1;
     }
 
     if (i2s_out->data_gpios[i] >= 0) {
-      gpio_ll_output_disable(&GPIO, i2s_out->data_gpios[i]);
+      disable_gpio_pin(i2s_out->data_gpios[i]);
 
       i2s_out->data_gpios[i] = -1;
     }
 
     if (i2s_out->inv_data_gpios[i] >= 0) {
-      gpio_ll_output_disable(&GPIO, i2s_out->inv_data_gpios[i]);
+      disable_gpio_pin(i2s_out->inv_data_gpios[i]);
 
       i2s_out->inv_data_gpios[i] = -1;
     }

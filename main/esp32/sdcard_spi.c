eafc667d35a2895ea39fa006d1fc6e62cfe26a15
Tero Marttila <terom@fixme.fi>
2023-03-15 22:01:22 +0200

    main: sdcard sdkconfig pins, max freq

--- main/esp32/sdcard_spi.c
+++ main/esp32/sdcard_spi.c
@@ -7,31 +7,26 @@
   #include <driver/sdspi_host.h>
   #include <driver/sdmmc_types.h>
 
-  // TODO: shared? conflict with leds_spi?
-  #define SDCARD_SPI_HOST SPI2_HOST
+  #define SDCARD_SPI_HOST SPI2_HOST // TODO: shared? conflict with leds_spi?
   #define SDCARD_SPI_DMA_CH SPI_DMA_CH_AUTO
-  #define SDCARD_SPI_MOSI_IO_NUM 15
-  #define SDCARD_SPI_MISO_IO_NUM 2
-  #define SDCARD_SPI_SCLK_IO_NUM 14
-  #define SDCARD_SPI_CS_IO_NUM 13
   #define SDCARD_SPI_MAX_TRANSFER_SZ 4000 // XXX: whence? One DMA buffer?
-  #define SDCARD_SPI_MAX_FREQ_KHZ 10000 // 10MHz for crappy breadboard testing
+  #define SDCARD_SPI_MAX_FREQ_KHZ CONFIG_SDCARD_SPI_MAX_FREQ_KHZ
 
   sdmmc_host_t sdcard_spi_host = SDSPI_HOST_DEFAULT();
 
   int init_sdcard_spi(sdmmc_host_t **hostp)
   {
     spi_bus_config_t bus_config = {
-      .mosi_io_num      = SDCARD_SPI_MOSI_IO_NUM,
-      .miso_io_num      = SDCARD_SPI_MISO_IO_NUM,
-      .sclk_io_num      = SDCARD_SPI_SCLK_IO_NUM,
+      .mosi_io_num      = CONFIG_SDCARD_SPI_MOSI_PIN,
+      .miso_io_num      = CONFIG_SDCARD_SPI_MISO_PIN,
+      .sclk_io_num      = CONFIG_SDCARD_SPI_SCLK_PIN,
       .quadwp_io_num    = -1,
       .quadhd_io_num    = -1,
       .max_transfer_sz  = SDCARD_SPI_MAX_TRANSFER_SZ,
     };
     sdspi_device_config_t dev_config = {
       .host_id          = SDCARD_SPI_HOST,
-      .gpio_cs          = SDCARD_SPI_CS_IO_NUM,
+      .gpio_cs          = CONFIG_SDCARD_SPI_CS_PIN,
       .gpio_cd          = SDSPI_SLOT_NO_CD,
       .gpio_wp          = SDSPI_SLOT_NO_WP,
       .gpio_int         = SDSPI_SLOT_NO_INT,

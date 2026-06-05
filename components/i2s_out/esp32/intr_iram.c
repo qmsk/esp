dd689829ee4034d444213d879a98ff7e80d0a63d
Tero Marttila <terom@qmsk.fi>
2026-06-05 12:16:54 +0300

    i2s_out: refactor intr state set

--- components/i2s_out/esp32/intr_iram.c
+++ components/i2s_out/esp32/intr_iram.c
@@ -6,7 +6,44 @@
 
 #include <logging.h>
 
-void i2s_intr_out_dscr_err_handler(struct i2s_out *i2s_out)
+// we may miss some EOF ISR for intermediate DMA descriptors, ensure we unblock i2s_out_dma_wait() on owner up to eof_desc
+static void i2s_out_intr_dma_eof_desc(struct i2s_out *i2s_out, struct dma_desc *eof_desc, BaseType_t *pxHigherPriorityTaskWoken)
+{
+  if (i2s_out->dma_eof_desc) {
+    for (struct dma_desc *desc = eof_desc; desc > i2s_out->dma_eof_desc; desc--) {
+      if (desc != eof_desc) {
+        LOG_ISR_WARN("miss desc=%p owner=%u len=%u", desc, desc->owner, desc->len);
+      }
+
+      i2s_dma_desc_reset(desc);
+    }
+  } else {
+    i2s_dma_desc_reset(eof_desc);
+  }
+
+  // unblock wait()
+  i2s_out->dma_eof_desc = eof_desc;
+
+  if (i2s_out->dma_eof_task) {
+    xTaskNotifyFromISR(i2s_out->dma_eof_task, I2S_OUT_TASK_NOTIFY_BIT_DMA_EOF, eSetBits, pxHigherPriorityTaskWoken);
+
+    i2s_out->dma_eof_task = NULL;
+  }
+}
+
+static void i2s_out_intr_dma_done(struct i2s_out *i2s_out, BaseType_t *pxHigherPriorityTaskWoken)
+{
+  // unblock flush()
+  i2s_out->dma_done = true;
+
+  if (i2s_out->dma_done_task) {
+      xTaskNotifyFromISR(i2s_out->dma_done_task, I2S_OUT_TASK_NOTIFY_BIT_DMA_DONE, eSetBits, pxHigherPriorityTaskWoken);
+
+      i2s_out->dma_done_task = NULL;
+  }
+}
+
+static void i2s_intr_out_dscr_err_handler(struct i2s_out *i2s_out)
 {
   uint32_t dscr_addr;
 
@@ -16,7 +53,7 @@ void i2s_intr_out_dscr_err_handler(struct i2s_out *i2s_out)
   LOG_ISR_ERROR("desc=%p", dscr_addr);
 }
 
-void i2s_intr_out_eof_handler(struct i2s_out *i2s_out, BaseType_t *pxHigherPriorityTaskWoken)
+static void i2s_intr_out_eof_handler(struct i2s_out *i2s_out, BaseType_t *pxHigherPriorityTaskWoken)
 {
   uint32_t eof_addr;
 
@@ -31,27 +68,7 @@ void i2s_intr_out_eof_handler(struct i2s_out *i2s_out, BaseType_t *pxHigherPrior
   if (eof_desc >= i2s_out->dma_out_desc && eof_desc < i2s_out->dma_out_desc + i2s_out->dma_out_count) {
     LOG_ISR_DEBUG("eof desc=%p owner=%u len=%u", eof_desc, eof_desc->owner, eof_desc->len);
 
-    // we may miss some EOF ISR for intermediate DMA descriptors, unblock i2s_out_dma_wait()
-    if (i2s_out->dma_eof_desc) {
-      for (struct dma_desc *desc = eof_desc; desc > i2s_out->dma_eof_desc; desc--) {
-        if (desc != eof_desc) {
-          LOG_ISR_WARN("miss desc=%p owner=%u len=%u", desc, desc->owner, desc->len);
-        }
-
-        i2s_dma_desc_reset(desc);
-      }
-    } else {
-      i2s_dma_desc_reset(eof_desc);
-    }
-
-    // unblock wait()
-    i2s_out->dma_eof_desc = eof_desc;
-
-    if (i2s_out->dma_eof_task) {
-        xTaskNotifyFromISR(i2s_out->dma_eof_task, I2S_OUT_TASK_NOTIFY_BIT_DMA_EOF, eSetBits, pxHigherPriorityTaskWoken);
-
-        i2s_out->dma_eof_task = NULL;
-    }
+    i2s_out_intr_dma_eof_desc(i2s_out, eof_desc, pxHigherPriorityTaskWoken);
 
   } else if (i2s_out->dma_repeat_desc && eof_desc >= i2s_out->dma_repeat_desc && eof_desc < i2s_out->dma_repeat_desc + i2s_out->dma_out_count * i2s_out->dma_repeat_count) {
     LOG_ISR_DEBUG("ignore repeat desc=%p owner=%u len=%u", eof_desc, eof_desc->owner, eof_desc->len);
@@ -61,35 +78,16 @@ void i2s_intr_out_eof_handler(struct i2s_out *i2s_out, BaseType_t *pxHigherPrior
     LOG_ISR_WARN("end desc=%p owner=%u len=%u, dma_write_desc=%p dma_eof_desc=%p", eof_desc, eof_desc->owner, eof_desc->len, i2s_out->dma_write_desc, i2s_out->dma_eof_desc);
 
     eof_desc = i2s_out->dma_out_desc + i2s_out->dma_out_count - 1;
-    
-    for (struct dma_desc *desc = eof_desc; desc > i2s_out->dma_eof_desc; desc--) {
-      i2s_dma_desc_reset(desc);
-    }
 
-    // unblock wait()
-    i2s_out->dma_eof_desc = eof_desc;
-
-    if (i2s_out->dma_eof_task) {
-        xTaskNotifyFromISR(i2s_out->dma_eof_task, I2S_OUT_TASK_NOTIFY_BIT_DMA_EOF, eSetBits, pxHigherPriorityTaskWoken);
-
-        i2s_out->dma_eof_task = NULL;
-    }
-
-    // unblock flush()
-    i2s_out->dma_done = true;
-
-    if (i2s_out->dma_done_task) {
-        xTaskNotifyFromISR(i2s_out->dma_done_task, I2S_OUT_TASK_NOTIFY_BIT_DMA_DONE, eSetBits, pxHigherPriorityTaskWoken);
-
-        i2s_out->dma_done_task = NULL;
-    }
+    i2s_out_intr_dma_eof_desc(i2s_out, eof_desc, pxHigherPriorityTaskWoken);
+    i2s_out_intr_dma_done(i2s_out, pxHigherPriorityTaskWoken);
 
   } else {
     LOG_ISR_ERROR("ignore desc=%p owner=%u len=%u", eof_desc, eof_desc->owner, eof_desc->len);
   }
 }
 
-void i2s_intr_tx_rempty_handler(struct i2s_out *i2s_out, BaseType_t *pxHigherPriorityTaskWoken)
+static void i2s_intr_tx_rempty_handler(struct i2s_out *i2s_out, BaseType_t *pxHigherPriorityTaskWoken)
 {
   LOG_ISR_DEBUG("");
 

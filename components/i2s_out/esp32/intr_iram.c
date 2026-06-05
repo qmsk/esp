41d951a8d794ffea2ead2c09ea71edb6a9c544fd
Tero Marttila <terom@qmsk.fi>
2026-06-05 14:11:39 +0300

    i2s_out: fix NULL deref on multiple EOF ISRs

--- components/i2s_out/esp32/intr_iram.c
+++ components/i2s_out/esp32/intr_iram.c
@@ -1,4 +1,4 @@
-#define ERROR 
+#define ERROR
 
 #include "../i2s_out.h"
 #include "dma.h"
@@ -10,7 +10,7 @@
 static void i2s_out_intr_dma_out_desc(struct i2s_out *i2s_out, struct dma_desc *eof_desc, BaseType_t *pxHigherPriorityTaskWoken)
 {
   // update dma_out_desc and reset descs for write
-  for (struct dma_desc *desc = i2s_out->dma_out_desc; desc != eof_desc; desc = desc->next) {
+  for (struct dma_desc *desc = i2s_out->dma_out_desc; (desc != NULL) && (desc != eof_desc); desc = desc->next) {
     LOG_ISR_WARN("missed desc=%p owner=%u len=%u", desc, desc->owner, desc->len);
 
     i2s_dma_desc_reset(desc);

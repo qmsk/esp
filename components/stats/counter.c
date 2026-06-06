58bedb58211897c03b96b64721a6104803024fab
Tero Marttila <terom@qmsk.fi>
2026-06-06 13:51:34 +0300

    stats: handle counter/time reset for metrics

--- components/stats/counter.c
+++ components/stats/counter.c
@@ -2,43 +2,44 @@
 
 struct stats_counter_metrics stats_counter_diff_metrics(const struct stats_counter *old, const struct stats_counter *new)
 {
-  if ((old && old->update > 0) && (new && new->update == old->update)) {
-    // increasing interval if updates stop
+  if ((!old->update || new->reset > old->reset) && !new->update) {
+    // increasing interval with zero rate until first update
     return (struct stats_counter_metrics) {
-      .interval = (float)(esp_timer_get_time() - old->update) / 1000000.0f,
+      .interval = (float)(esp_timer_get_time() - new->reset) / 1000000.0f,
     };
-  } else if ((old && old->update > 0) && (new && new->update > old->update)) {
-    // incremental update
-    return (struct stats_counter_metrics) {
-      .interval = (float)(new->update - old->update) / 1000000.0f,
-      .rate = ((float) (new->count - old->count)) / ((float)(new->update - old->update) / 1000000.0f),
-    };
-  } else if (!(old && old->update) && (new && new->update > new->reset)) {
+  } else if ((!old->update || new->reset > old->reset) && (new->update > new->reset)) {
     // initial update
     return (struct stats_counter_metrics) {
       .interval = (float)(new->update - new->reset) / 1000000.0f,
       .rate = ((float) (new->count)) / ((float)(new->update - new->reset) / 1000000.0f),
     };
-  } else if (!(old && old->update) && (new && new->reset)) {
-    // increasing interval until first update
+  } else if ((old->update && old->reset == new->reset) && new->update == old->update) {
+    // increasing interval with zero rate if updates stop
     return (struct stats_counter_metrics) {
-      .interval = (float)(esp_timer_get_time() - new->reset) / 1000000.0f,
+      .interval = (float)(esp_timer_get_time() - old->update) / 1000000.0f,
+    };
+  } else if ((old->update && old->reset == new->reset) && new->update > old->update) {
+    // incremental update
+    return (struct stats_counter_metrics) {
+      .interval = (float)(new->update - old->update) / 1000000.0f,
+      .rate = ((float) (new->count - old->count)) / ((float)(new->update - old->update) / 1000000.0f),
     };
   } else {
+    // ???
     return (struct stats_counter_metrics) {};
   }
 }
 
 struct stats_counter_metrics stats_counter_metrics_average(const struct stats_counter_metrics *old, const struct stats_counter_metrics *new)
 {
-  if ((old && old->interval) && new) {
+  if (old->interval && new->interval) {
     return (struct stats_counter_metrics) {
       .interval = (old->interval + new->interval) / 2,
       .rate = (old->rate + new->rate) / 2,
     };
-  } else if ((old && old->interval) && !(new && new->interval)) {
+  } else if (old->interval && !new->interval) {
     return *old;
-  } else if (!(old && old->interval) && (new && new->interval)) {
+  } else if (!old->interval && new->interval) {
     return *new;
   } else {
     return (struct stats_counter_metrics) {};
--- components/stats/timer.c
+++ components/stats/timer.c
@@ -2,46 +2,47 @@
 
 struct stats_timer_metrics stats_timer_diff_metrics(const struct stats_timer *old, const struct stats_timer *new)
 {
-  if ((old && old->update > 0) && (new && new->update == old->update)) {
-    // increasing interval if updates stop
+    if ((!old->update || new->reset > old->reset) && !new->update) {
+    // increasing interval with zero rate until first update
     return (struct stats_timer_metrics) {
-      .interval = (float)(esp_timer_get_time() - old->update) / 1000000.0f,
+      .interval = (float)(esp_timer_get_time() - new->reset) / 1000000.0f,
     };
-  } else if ((old && old->update > 0) && (new && new->update > old->update)) {
-    // incremental update
-    return (struct stats_timer_metrics) {
-      .interval = (float)(new->update - old->update) / 1000000.0f,
-      .rate = ((float) (new->count - old->count)) / ((float)(new->update - old->update) / 1000000.0f),
-      .util = ((float) (new->total - old->total)) / ((float)(new->update - old->update)),
-    };
-  } else if (!(old && old->update) && (new && new->update > new->reset)) {
+  } else if ((!old->update || new->reset > old->reset) && (new->update > new->reset)) {
     // initial update
     return (struct stats_timer_metrics) {
       .interval = (float)(new->update - new->reset) / 1000000.0f,
       .rate = ((float) (new->count)) / ((float)(new->update - new->reset) / 1000000.0f),
       .util = ((float) (new->total)) / ((float)(new->update - new->reset)),
     };
-  } else if (!(old && old->update) && (new && new->reset)) {
-    // increasing interval until first update
+  } else if ((old->update && old->reset == new->reset) && new->update == old->update) {
+    // increasing interval with zero rate if updates stop
     return (struct stats_timer_metrics) {
-      .interval = (float)(esp_timer_get_time() - new->reset) / 1000000.0f,
+      .interval = (float)(esp_timer_get_time() - old->update) / 1000000.0f,
+    };
+  } else if ((old->update && old->reset == new->reset) && new->update > old->update) {
+    // incremental update
+    return (struct stats_timer_metrics) {
+      .interval = (float)(new->update - old->update) / 1000000.0f,
+      .rate = ((float) (new->count - old->count)) / ((float)(new->update - old->update) / 1000000.0f),
+      .util = ((float) (new->total - old->total)) / ((float)(new->update - old->update)),
     };
   } else {
+    // ???
     return (struct stats_timer_metrics) {};
   }
 }
 
 struct stats_timer_metrics stats_timer_metrics_average(const struct stats_timer_metrics *old, const struct stats_timer_metrics *new)
 {
-  if ((old && old->interval) && new) {
+  if (old->interval && new->interval) {
     return (struct stats_timer_metrics) {
       .interval = (old->interval + new->interval) / 2,
       .rate = (old->rate + new->rate) / 2,
       .util = (old->util + new->util) / 2,
     };
-  } else if ((old && old->interval) && !(new && new->interval)) {
+  } else if (old->interval && !new->interval) {
     return *old;
-  } else if (!(old && old->interval) && (new && new->interval)) {
+  } else if (!old->interval && new->interval) {
     return *new;
   } else {
     return (struct stats_timer_metrics) {};

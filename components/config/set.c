aab96ee106b05e8825da4d2b60eb6109088a041a
Tero Marttila <terom@qmsk.fi>
2026-06-13 16:53:15 +0300

    config_color_parse

--- components/config/set.c
+++ components/config/set.c
@@ -167,45 +167,9 @@ static int config_set_file(const struct config_path path, unsigned index, const 
 static int config_set_color(const struct config_path path, unsigned index, const char *str)
 {
   const struct configtab *tab = path.tab;
-  int r = 0, g = 0, b = 0, a = 0;
+  int err;
 
-  switch (strlen(str)) {
-    case 3:
-      a = 0xff;
-
-      if (sscanf(str, "%1x%1x%1x", &r, &g, &b) < 3) {
-        return -1;
-      }
-
-      r = (r << 4) | r;
-      g = (g << 4) | g;
-      b = (b << 4) | b;
-
-      break;
-
-    case 6:
-      a = 0xff;
-      
-      if (sscanf(str, "%2x%2x%2x", &r, &g, &b) < 3) {
-        return -1;
-      }
-
-      break;
-
-    case 8:
-      if (sscanf(str, "%2x%2x%2x%2x", &r, &g, &b, &a) < 4) {
-        return -1;
-      }
-
-      break;
-    
-    default:
-      return -1;
-  }
-
-  tab->color_type.value[index] = (struct config_color) { r, g, b, a };
-
-  return 0;
+  return config_color_parse(&tab->color_type.value[index], str);
 }
 
 int config_set(const struct config_path path, const char *value)

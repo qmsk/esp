c53bcfffc908a15cbe1ce007a726a608f510a632
Tero Marttila <terom@qmsk.fi>
2026-06-13 17:17:21 +0300

    leds: include static.color in leds http

--- main/leds_http.c
+++ main/leds_http.c
@@ -16,6 +16,21 @@
 
 #define TICK_MS(current_tick, tick) (tick ? (current_tick - tick) * portTICK_RATE_MS : 0)
 
+static int leds_api_write_leds_color(struct json_writer *w, struct leds_color c, enum leds_parameter_type parameter_type)
+{
+  switch (parameter_type) {
+    case LEDS_PARAMETER_NONE:
+      return json_write_raw(w, "\"%02x%02x%02x\"", c.r, c.g, c.b);
+
+    case LEDS_PARAMETER_DIMMER:
+    case LEDS_PARAMETER_WHITE:
+      return json_write_raw(w, "\"%02x%02x%02x.%02x\"", c.r, c.g, c.b, c.parameter);
+
+    default:
+      LOG_FATAL("%d", parameter_type);
+  }
+}
+
 static int leds_api_write_object_options(struct json_writer *w, struct leds_state *state)
 {
   const struct leds_options *options = leds_options(state->leds);
@@ -105,6 +120,9 @@ static int leds_api_write_object(struct json_writer *w, struct leds_state *state
         JSON_WRITE_MEMBER_UINT(w, "index", state->index + 1)
     ||  JSON_WRITE_MEMBER_OBJECT(w, "options", leds_api_write_object_options(w, state))
     ||  JSON_WRITE_MEMBER_OBJECT(w, "status", leds_api_write_object_status(w, state))
+    ||  JSON_WRITE_MEMBER_OBJECT(w, "static",
+          JSON_WRITE_MEMBER(w, "color", leds_api_write_leds_color(w, state->static_.color, leds_parameter_type_for_protocol(leds_protocol(state->leds))))
+        )
     ||  (state->artnet ? JSON_WRITE_MEMBER_OBJECT(w, "artnet", leds_api_write_object_artnet(w, state)) : 0)
   );
 }
